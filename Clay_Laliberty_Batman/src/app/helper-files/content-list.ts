f9248bef8342140e2165b7c41db3db29f5751f92
Clay <W0673373@myscc.ca>
2024-01-28 20:13:50 -0500

    Assignment-2

--- Clay_Laliberty_Batman/src/app/helper-files/content-list.ts
+++ Clay_Laliberty_Batman/src/app/helper-files/content-list.ts
@@ -1,49 +1,49 @@
-// Content-list
+// // Content-list
 
-import { Content } from "./content-interface";
+// import { Content } from "./content-interface";
 
 
-export class ContentList {
+// export class ContentList {
     
-    private contentListItems: Content[];  // Array renamed as contentListItems
+//     private contentListItems: Content[];  // Array renamed as contentListItems
 
-    constructor() {    // Contructor for ContentList class
-        this.contentListItems = [];   // initialize array
-    }
+//     constructor() {    // Contructor for ContentList class
+//         this.contentListItems = [];   // initialize array
+//     }
 
-    get items(): Content[] {    // Provides a getter method for items
-        return this.contentListItems;
-    }
+//     get items(): Content[] {    // Provides a getter method for items
+//         return this.contentListItems;
+//     }
     
-    add(item: Content): void {  // add method to add the content properties 
-        this.contentListItems.push(item);
-    }
+//     add(item: Content): void {  // add method to add the content properties 
+//         this.contentListItems.push(item);
+//     }
 
-    getCount(): number {    // method to return # of items  
-        return this.contentListItems.length;
-    }
+//     getCount(): number {    // method to return # of items  
+//         return this.contentListItems.length;
+//     }
 
-    displayContent(index: number): string { // Reader-freindly html output of content properties
-        const mainContent = this.contentListItems[index];
+//     displayContent(index: number): string { // Reader-freindly html output of content properties
+//         const mainContent = this.contentListItems[index];
 
-        if(!mainContent) {
-            return 'Invaild index👎'
-        }
+//         if(!mainContent) {
+//             return 'Invaild index👎'
+//         }
 
-        const imgTag = mainContent.imgURL ? `<img src="${mainContent.imgURL}" alt="Content Image">` : '';
-
-
-        return `
-        <div>
-            <h2>${mainContent.title}</h2>
-            <p>${mainContent.description}</p>
-            <p>Creator: ${mainContent.creator}</p>
-            ${imgTag}
-            <p>Type: ${mainContent.type}</p>
-        </div>
-        `;
-
-    }
+//         const imgTag = mainContent.imgURL ? `<img src="${mainContent.imgURL}" alt="Content Image">` : '';
 
 
-}
+//         return `
+//         <div>
+//             <h2>${mainContent.title}</h2>
+//             <p>${mainContent.description}</p>
+//             <p>Creator: ${mainContent.creator}</p>
+//             ${imgTag}
+//             <p>Type: ${mainContent.type}</p>
+//         </div>
+//         `;
+
+//     }
+
+
+// }
