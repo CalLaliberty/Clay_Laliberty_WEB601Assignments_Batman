7f357e47c7d215c011f477d9c94acff50f66a8b9
Clay <W0673373@myscc.ca>
2024-03-10 14:10:54 -0400

    Assignment-6

--- Clay_Laliberty_Batman/src/app/app.component.ts
+++ Clay_Laliberty_Batman/src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContentListComponent } from './content-list/content-list.component';
-// import { ContentCardComponent } from "./content-card/content-card.component";
+import { MessagesComponent } from './messages/messages.component';
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.sass',
-    imports: [CommonModule, RouterOutlet, ContentListComponent] // , ContentCardComponent
+    imports: [CommonModule, RouterOutlet, ContentListComponent, MessagesComponent] 
 })
 export class AppComponent {
   title = 'Clay_Laliberty_Batman';
